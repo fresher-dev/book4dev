71127c4868290ef0a08612410cbdd51cd048372d
fresher-dev <www.iceface@gmail.com>
2023-01-27 09:36:39 +0630

    add fix some broken link

--- api/views.py
+++ api/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from .serializers import BookSerializers, BlogSerializers
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from rest_framework.response import Response
-from rest_framework import status
 from rest_framework import permissions
 from book.models import Book
 from blog.models import Blog
@@ -11,11 +9,12 @@ from blog.models import Blog
 class BookListApiView(ListCreateAPIView):
 	# add permission to check if user if authenticated
 	permission_classes = [permissions.IsAuthenticated]
-
 	serializer_class = BookSerializers
 	queryset = Book.objects.all()
 
 class BookUpdateApiView(RetrieveUpdateDestroyAPIView):
+	# add permission to check if user if authenticated
+	permission_classes = [permissions.IsAuthenticated]
 	serializer_class = BookSerializers
 	queryset = Book.objects.all()
 
@@ -23,11 +22,12 @@ class BookUpdateApiView(RetrieveUpdateDestroyAPIView):
 class BlogListApiView(ListCreateAPIView):
 	# add permission to check if user if authenticated
 	permission_classes = [permissions.IsAuthenticated]
-
 	serializer_class = BlogSerializers
 	queryset = Blog.objects.all()
 
 class BlogUpdateApiView(RetrieveUpdateDestroyAPIView):
+	# add permission to check if user if authenticated
+	permission_classes = [permissions.IsAuthenticated]
 	serializer_class = BlogSerializers
 	queryset = Blog.objects.all()
 
